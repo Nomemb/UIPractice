6f328966f3a7cf6eedf000028b59226dbbea7a5e
Nomemb <nkj1205@gmail.com>
2023-12-14 16:35:28 +0900

    feat : "UI_Base 구현"
    모든 UI들의 부모 클래스인 UI_Base 클래스 구현

---
+++ Assets/Scripts/UI/UI_Base.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class UI_Base : MonoBehaviour
+{
+    protected Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+    protected bool _init = false;
+
+    public virtual bool Init()
+    {
+        if (_init)
+            return false;
+
+        return _init = true;
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    protected void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (typeof(T) == typeof(GameObject))
+                objects[i] = Utils.FindChild(gameObject, names[i], true);
+            else
+                objects[i] = Utils.FindChild<T>(gameObject, names[i], true);
+            
+            if (objects[i] == null)
+                Debug.Log($"Failed to bind({names[i]}");
+            
+            Debug.Log(objects[i].name);
+        }
+    }
+
+    protected void BindText(Type type) { Bind<TextMeshProUGUI>(type); }
+
+    protected void BindButton(Type type) { Bind<Button>(type); }
+
+    protected T Get<T>(int idx) where T : UnityEngine.Object
+    {
+        UnityEngine.Object[] objects = null;
+        if (_objects.TryGetValue(typeof(T), out objects) == false)
+            return null;
+
+        return objects[idx] as T;
+    }
+
+    protected Button GetButton(int idx) { return Get<Button>(idx); }
+
+    public static void BindEvent(GameObject go, Action action, Define.UIEvent type = Define.UIEvent.Click)
+    {
+        UI_EventHandler evt = Utils.GetOrAddComponent<UI_EventHandler>(go);
+
+        switch (type)
+        {
+            case Define.UIEvent.Click:
+                evt.OnClickHandler -= action;
+                evt.OnClickHandler += action;
+                break;
+            case Define.UIEvent.Pressed:
+                evt.OnPressedHandler -= action;
+                evt.OnPressedHandler += action;
+                break;
+            case Define.UIEvent.PointerDown:
+                evt.OnPointerDownHandler -= action;
+                evt.OnPointerDownHandler += action;
+                break;
+            case Define.UIEvent.PointerUp:
+                evt.OnPointerUpHandler -= action;
+                evt.OnPointerUpHandler += action;
+                break;
+        }
+    }
+}
